e42b5ba9358b7e882585e530751970251b16a5d9
sadiq1971 <sadiqurr8@gmail.com>
2023-02-23 00:40:45 +0600

    updated dependency

--- bundler_test.go
+++ bundler_test.go
@@ -9,7 +9,7 @@ import (
 	//	"time"
 
 	//	"github.com/streamingfast/bstream"
-	//"github.com/streamingfast/merger/bundle"
+	//"github.com/streamingfast/sadia1971/bundle"
 	"github.com/streamingfast/bstream"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
